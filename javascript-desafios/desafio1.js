b59c05e67c1754022f726ef037481cc145c2cb89
Daniel Olivier <danielolivier8@hotmail.com>
2022-02-18 12:05:41 -0300

    correccion de desafio1

--- javascript-desafios/desafio1.js
+++ javascript-desafios/desafio1.js
@@ -2,42 +2,48 @@ class Usuario {
     constructor(nombre, apellido, libros, mascotas){
         this.nombre = nombre
         this.apellido = apellido
-        this.libros = libros = [
-            {nombre:'El senor de las moscas',
-            autor:'William Golding'},
-            {nombre:'Fundacion',
-            autor:'Isaac Asimov'}
-        ]
-        this.mascotas = mascotas = ['Perro', 'Gato']
+        this.libros = libros = []
+        this.mascotas = mascotas = []
     }
 
-    addMascota(){
-        this.mascotas.push('Vaca')
-       return this.mascotas
-    }
+    addMascota = ['Perro', 'Gato', "Vaca"]
+    
 
     countMascotas() {
-      return this.mascotas.length
+            this.mascotas = this.addMascota
+        return this.mascotas.length
     }
 
-    getBooks() { 
-        return this.libros = [
-            'El senor de las moscas', 'Fundacion' 
-       ]
+    addBook = [  
+        {nombre:'El senor de las moscas',
+        autor:'William Golding'},
+
+        {nombre:'Fundacion',
+        autor:'Isaac Asimov'}]
+
+    getBookNames() {
+        this.libros = this.addBook
+      return this.libros = [{nombre:'El senor de las moscas'},{nombre:'Fundacion'}]
     }
+    
 
     getFullName() {
         return `Bienvenido ${this.nombre}${this.apellido}`
     }
 }
 
+
+
 const usuario1 = new Usuario ('Daniel',' Olivier')
 console.log(usuario1.getFullName());
 
 const usuario2 = new Usuario ()
-const usuario4 = new Usuario ()
-console.log(usuario4.addMascota());
-console.log(usuario2.countMascotas());
+console.log(usuario2.addMascota);
+const usuario3 = new Usuario ()
+console.log(usuario3.countMascotas());
 
-const usuario3 = new Usuario ();
-console.log(usuario3.getBooks());
+const usuario4 = new Usuario ()
+console.log(usuario4.getBookNames());
+const usuario5 = new Usuario ()
+console.log(usuario5.addBook);
+
